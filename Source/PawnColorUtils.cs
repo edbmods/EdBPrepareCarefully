4e2e05e21fbe6dab8f249c9daa6705cd1796b873
edbmods <edbmods@gmail.com>
2016-09-25 19:56:24 -0700

    Updated PawnColorUtils with an indirect method for determining the colors from PawnSkinColors.  This approach may work better with mods that have detours methods in the PawnSkinColors class.

--- Source/PawnColorUtils.cs
+++ Source/PawnColorUtils.cs
@@ -9,16 +9,9 @@ namespace EdB.PrepareCarefully
 {
 	public class PawnColorUtils
 	{
+		// The values will match the color values in the PawnSkinColors class.  We use a round-about way of getting at
+		// those color values in the InitializeColors() method.
 		public static Color[] Colors;
-		// The values are copied from the PawnSkinColors class, but after being copied they will look something like this.
-		//public static Color[] Colors = new Color[] {
-		//	new Color(0.3882353f, 0.2745098f, 0.1411765f),
-		//	new Color(0.509804f, 0.3568628f, 0.1882353f),
-		//	new Color(0.8941177f, 0.6196079f, 0.3529412f),
-		//	new Color(1f, 0.9372549f, 0.7411765f),
-		//	new Color(1f, 0.9372549f, 0.8352941f),
-		//	new Color(0.9490196f, 0.9294118f, 0.8784314f),
-		//};
 
 		// The same colors, but rounded to 3 fractional digits.  When colors are saved to a preset, they are
 		// automatically rounded, so we need to use these rounded values when loading them.  We could avoid this if
@@ -27,36 +20,55 @@ namespace EdB.PrepareCarefully
 		// backwards-compatibile.
 		public static Color[] RoundedColors;
 
-		// The values are copied from the PawnSkinColors class, but after being copied they will look something like this.
-		public static float[] ColorValues; // = new float[] { 0f, 0.1f, 0.25f, 0.5f, 0.75f, 1f };
+		// The values will match the color values in the PawnSkinColors class.
+		public static float[] ColorValues;
 
-		// The values are copied from the PawnSkinColors class, but after being copied they will look something like this.
-		public static float[] ColorSelectors; // = new float[] { 0f, 0.05f, 0.2f, 0.285f, 0.785f, 1f };
-
-		// Populates color arrays from PawnSkinColors.SkinColors.  Uses reflection because the field that stores the
-		// colors and the SkinColorData class are both private.
+		// Populates color arrays from PawnSkinColors.SkinColors.  Uses an indirect method of getting the color values
+		// instead of just copying the color list via reflection.  This will make it work better with mods that
+		// detour the color methods in that class--as long as they detour the GetSkinDataLeftIndexByWhiteness() method.
 		public static void InitializeColors()
 		{
-			FieldInfo skinColorsField = typeof(PawnSkinColors).GetField("SkinColors", BindingFlags.Static | BindingFlags.NonPublic);
-			Array skinColors = (Array)skinColorsField.GetValue(null);
-			int length = skinColors.Length;
+			List<float> values = new List<float>();
+
+			// Get the private GetSkinDataLeftIndexByWhiteness() method from the PawnSkinColors class.
+			MethodInfo getSkinDataLeftIndexByWhitenessMethod = typeof(PawnSkinColors)
+					.GetMethod("GetSkinDataLeftIndexByWhiteness",
+				    BindingFlags.NonPublic | BindingFlags.Static, null, new Type[] { typeof(float) }, null);
+
+			// Iterate all values from 0.0f to 1.0f, using increments of 0.01f, to get the left index for each value.
+			// Use this technique to construct a list of all of the indexes and their values.  Once we have the list
+			// of indexes and their values, we can use the GetSkinColor() method to get the actual colors.
+			int currentIndex = 0;
+			values.Add(0.0f);
+			float f = 0.01f;
+			int counter = 1;
+			while (f < 1.0f) {
+				int result = (int) getSkinDataLeftIndexByWhitenessMethod.Invoke(null, new object[] { f });
+				if (result != currentIndex) {
+					currentIndex = result;
+					values.Add(f);
+				}
+				counter++;
+				double d = (double)counter / 100.0;
+				f = (float)d;
+			}
+			values.Add(1.0f);
+
+			// Allocate the arrays and fill them with the correct values.
+			int length = values.Count;
 			Colors = new Color[length];
-			RoundedColors = new Color[length];
 			ColorValues = new float[length];
-			ColorSelectors = new float[length];
+			RoundedColors = new Color[length];
 			for (int i = 0; i < length; i++) {
-				object colorData = skinColors.GetValue(i);
-				FieldInfo whitenessField = colorData.GetType().GetField("whiteness", BindingFlags.Instance | BindingFlags.Public);
-				FieldInfo selectorField = colorData.GetType().GetField("selector", BindingFlags.Instance | BindingFlags.Public);
-				FieldInfo colorField = colorData.GetType().GetField("color", BindingFlags.Instance | BindingFlags.Public);
-				Colors[i] = (Color)colorField.GetValue(colorData);
-				RoundedColors[i] = (Color)colorField.GetValue(colorData);
-				RoundedColors[i].r = (float)Math.Round(RoundedColors[i].r, 3);
-				RoundedColors[i].g = (float)Math.Round(RoundedColors[i].g, 3);
-				RoundedColors[i].b = (float)Math.Round(RoundedColors[i].b, 3);
-				RoundedColors[i].a = (float)Math.Round(RoundedColors[i].a, 3);
-				ColorValues[i] = (float)whitenessField.GetValue(colorData);
-				ColorSelectors[i] = (float)selectorField.GetValue(colorData);
+				float v = values[i];
+				Color color = PawnSkinColors.GetSkinColor(v);
+				Colors[i] = color;
+				RoundedColors[i] = color;
+				RoundedColors[i].r = (float)Math.Round(color.r, 3);
+				RoundedColors[i].g = (float)Math.Round(color.g, 3);
+				RoundedColors[i].b = (float)Math.Round(color.b, 3);
+				RoundedColors[i].a = (float)Math.Round(color.a, 3);
+				ColorValues[i] = v;
 			}
 		}
 
