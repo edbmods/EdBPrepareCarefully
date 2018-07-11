937041dd3d3d5e5abaa595b2d5fa010044301145
edbmods <edbmods@gmail.com>
2018-07-10 18:10:30 -0700

    Added ReflectionUtil that was missed in previous commit

---
+++ Source/ReflectionUtil.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+
+namespace EdB.PrepareCarefully {
+    public class ReflectionUtil {
+        public static FieldInfo GetNonPublicField(Type type, string name) {
+            FieldInfo info = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            if (info == null) {
+                Log.Warning("Prepare Carefully could not find the field " + type.Name + "." + name + " via reflection");
+            }
+            return info;
+        }
+
+        public static void SetNonPublicField(object target, string name, object value) {
+            FieldInfo info = GetNonPublicField(target.GetType(), name);
+            if (info == null) {
+                Log.Warning("Prepare Carefully failed to set a value via reflection");
+            }
+            else {
+                info.SetValue(target, value);
+            }
+        }
+    }
+}
