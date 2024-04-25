952743667b4fc2a5b40acdef1040c7ed3cc9d433
edbmods <edbmods@gmail.com>
2024-04-24 20:54:37 -0700

    Updated version and changelog for 1.5.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.1.1")]
 
 // Increment for each new release
-[assembly: AssemblyFileVersion("1.5.2")]
+[assembly: AssemblyFileVersion("1.5.3")]
